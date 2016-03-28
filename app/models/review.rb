06531e45b19c665d7916c0492dc2fca546781cc9
firusfaizal <firusfaizal@gmail.com>
2016-03-28 11:30:36 +0800

    add_review_user

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,13 @@
 class Review < ActiveRecord::Base
+    #Reviews belong to movies
+    belong_to :movies
+    
+    
+    #they also belong to users
+    belong_to :users
+    
+    
+
+    
+    
 end
