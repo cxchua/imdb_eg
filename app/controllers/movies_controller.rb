a44cfaa795e3514f4325460bdf71efce98947600
firusfaizal <firusfaizal@gmail.com>
2016-03-28 12:24:52 +0800

    added_student_2_controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,34 @@
 class MoviesController < ApplicationController
 
+    
+    def index
+        @users = Users.all
+    end
+    
+    
+    def show
+        
+    end
+    
+    
+    def new
+    
+    end
+    
+    
+    def create
+        
+    end
+    
+    
+    def edit
+        
+    end
+    
+    
+    def update
+        
+    end
+    
+    
 end
