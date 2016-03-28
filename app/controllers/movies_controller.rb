3ad3bbcf05b3e20c0e08c8f67ebd79244ab4edd1
firusfaizal <firusfaizal@gmail.com>
2016-03-28 13:36:31 +0800

    fixed movie-controllers

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,28 +7,29 @@ class MoviesController < ApplicationController
     
     
     def show
-        
     end
     
     
     def new
-    
     end
     
     
     def create
-        
     end
     
     
     def edit
-        
     end
     
     
     def update
-        
     end
     
+    private
+    def movie_params
+        params.require (:title).permit(:summary, :youtube_embeded_id, :thumbnail)
     
 end
+
+
+
